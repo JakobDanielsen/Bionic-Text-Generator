7a63cd706c48f3cb739fcd7a9b16207dfea65ab1
© Jakob 3301™ <danielsen.jakob@gmail.com>
2023-03-14 22:55:22 +0100

    it works! just need some cleanups in code now

--- fun.js
+++ fun.js
@@ -1,35 +1,39 @@
 document.getElementById("tField").addEventListener("submit",(event) => {
     event.preventDefault()
+    translate(document.getElementById("tArea").value)
 })
 
-const e = require('express');
+// const e = require('express');
 // import the readline-sync module
-const readlineSync = require('readline-sync');
+// const readlineSync = require('readline-sync');
 
 // prompt the user for input and store the result in a variable
-const text = readlineSync.question('Paste the text you would like to bionify: ');
+// const text = readlineSync.question('Paste the text you would like to bionify: ');
+function translate(text){
+    console.log(text);
+        // do something with the input
+    let wordArray = text.split(" ")
+    let finishedText = ""
+    // console.log(wordArray);
+    wordArray.forEach(e => {
+        let mid
+        let currentWord
+        // console.log(e);
+        // console.log(e.length);
+        if (e.length%2===0) {
+            mid = e.length/2
+        } else {
+            mid = (e.length+1)/2
+        }
+        // console.log(mid);
+        currentWord = "<b>".concat(e.slice(0,mid)).concat("","</b>").concat(e.slice(mid,e.length))
+        // console.log(currentWord); // THIS IS PROBABLY EXTREMELY INEFFICIENT BUT IT WORKS
+        finishedText = finishedText + currentWord.concat("", " ")
+        document.getElementById("bArea").innerHTML = finishedText
+    });
 
+    // console.log(`MIDWAY:${wordArray}`);
+    console.log(`FINISHED:${finishedText}`);
+}
 
-// do something with the input
-let wordArray = text.split(" ")
-let finishedText = ""
-console.log(wordArray);
-wordArray.forEach(e => {
-    let mid
-    let currentWord
-    console.log(e);
-    console.log(e.length);
-    if (e.length%2===0) {
-        mid = e.length/2
-    } else {
-        mid = (e.length+1)/2
-    }
-    console.log(mid);
-    currentWord = "*".concat(e.slice(0,mid)).concat("","*").concat(e.slice(mid,e.length))
-    console.log(currentWord); // THIS IS PROBABLY EXTREMELY INEFFICIENT BUT IT WORKS
-    finishedText = finishedText + currentWord.concat("", " ")
-});
-
-console.log(`MIDWAY:${wordArray}`);
-console.log(`FINISHED:${finishedText}`);
 
